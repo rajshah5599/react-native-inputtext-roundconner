90c06e93b6424b7bcaeff7d497e96ddcf82ee9f0
Rshah <rshah@universalstreamsolution.com>
2023-12-22 14:45:27 +0530

    Code optimize

--- Validation/index.tsx
+++ Validation/index.tsx
@@ -1,10 +1,15 @@
-import { trim } from 'lodash'
+// Custom trim function to remove leading and trailing whitespaces
+const customTrim = (str) => {
+    return str.replace(/^\s+|\s+$/g, '');
+};
 
-export const isEmailValid = (email: string) => {
+// Email validation function using the custom trim
+export const isEmailValid = (email) => {
     let pattern =
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return pattern.test(String(trim(email)).toLowerCase())
-}
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // Use customTrim
+    return pattern.test(String(customTrim(email)).toLowerCase());
+};
 
 export const VPasswordCheck = (Password: string) => {
     if (Password.length >= 6) {
